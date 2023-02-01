abfb7ad10fd0eaab596ceba2005b2de37ad769ef
mio2smug <bryan.lazarowork@yahoo.com>
2023-01-31 22:16:55 -0500

    html and css styles completed

---
+++ main.js
@@ -0,0 +1,25 @@
+//wor
+let userInput = [];
+
+
+function inputToInt(x){
+    userInput = parseInt(x);
+    return userInput;
+}
+
+function addition(x,y){
+    return x+y;
+}
+
+function subtraction(x,y){
+    return x - y;
+}
+
+function multiplication (x,y){
+    return x*y;
+}
+
+function division (x,y){
+    return x/y;
+}
+
