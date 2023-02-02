c11a8d4779c7ace208c7b2349168dd779ce10768
mio2smug <bryan.lazarowork@yahoo.com>
2023-02-02 16:09:27 -0500

    got the addition to sort of work

--- main.js
+++ main.js
@@ -1,39 +1,115 @@
 //Let user input equal an array
 let userInput = [];
+let prevuserInputs = [];
 let userInputToString;
 let userInputToInt;
 let userInputToFloat;
 let isUserInputFloat = false;
-let initialOperator ;
-let subsequentOperator ;
+let initialOperand ;
+let subsequentOperand ;
+let symbol = "";
 let counter = 0;
+
+/*function add(){
+    if (isUserInputFloat) {initialOperator = userInputToFloat;}
+    if (!isUserInputFloat) {initialOperator = userInputToInt;}
+    symbol = "+"
+    console.log(addition(initialOperator, 2.1 ));
+}
+
+function subtract(){
+    symbol = "-"
+    console.log(symbol);
+}
+
+function multiply(){
+    symbol = "*"
+    console.log(symbol);
+}
+
+function divide(){
+    symbol = "/"
+    console.log(symbol);
+}*/
+
+
+
+/*What should happen when this function calls?
+user has already inputed a number
+when user presses enter
+
+*/
+
+/*function enter(){
+     (isUserInputFloat){
+        initialOperand = userInputToFloat;
+        document.getElementById("operand").innerHTML = initialOperand;
+        userInput.splice(0, userInput.length);
+        document.getElementById("number").innerHTML = userInput.join('');
+        return 0;
+    }
+    initialOperand = userInputToInt;
+    document.getElementById("operand").innerHTML = initialOperand;
+    userInput.splice(0, userInput.length);
+    document.getElementById("number").innerHTML =  userInput.join('');
+    return 0;
+}*/
+
+/*when user enters, counter will be at 0 by default, this means that it will go to set
+function where it will set our initial Operator. Lets test using console log = Working.
+
+
+*/
+
+function add(){
+    if (counter === 0){ enter();return 0;}
+    setContinuous();
+    document.getElementById("number").innerHTML = addition(initialOperand,subsequentOperand);
+    
+}
+
+function enter(){
+    if (counter === 0){ set(); return 0;}
+    else{
+        setContinuous();
+    }
+}
+
+function setContinuous(){
+    if (isUserInputFloat){subsequentOperand = userInputToFloat; return 0;}
+    subsequentOperand = userInputToInt;
+    return 0;
+}
+
+function set (){
+    if (isUserInputFloat){ 
+        initialOperand = userInputToFloat; 
+        document.getElementById("operand").innerHTML = initialOperand;
+        userInput.splice(0, userInput.length);
+        document.getElementById("number").innerHTML =  userInput.join('');
+        counter++;
+        return 0;
+    }
+    initialOperand = userInputToInt; 
+    document.getElementById("operand").innerHTML = initialOperand;
+    userInput.splice(0, userInput.length);
+    document.getElementById("number").innerHTML =  userInput.join('');
+    counter++
+    return 0;
+}
+
+
 
 function removeall(){
     userInput.splice(0, userInput.length);
     document.getElementById("number").innerHTML = userInput.join('');
     document.getElementById("neg").disabled = false;
     document.getElementById("float").disabled = false;
-    initialOperator = 0;
-    subsequentOperator = 0;
+    isUserInputFloat = false;
+    initialOperand = 0;
+    subsequentOperand = 0;
     counter = 0;
     document.getElementById("operand").innerHTML = "";
-}
-
-function enter(){
-    counter++;
-    if (isUserInputFloat){
-        initialOperator = userInputToFloat;
-        document.getElementById("operand").innerHTML = initialOperator;
-        userInput.splice(0, userInput.length);
-        document.getElementById("number").innerHTML = userInput.join('');
-        return 0;
-    }
-    initialOperator = userInputToInt;
-    document.getElementById("operand").innerHTML = initialOperator;
-    userInput.splice(0, userInput.length);
-    document.getElementById("number").innerHTML =  userInput.join('');
-    console.log(counter);
-    return 0;
 }
 
 function remove(){
@@ -106,8 +182,7 @@ function pushZero(){
 
 function display(){
     userInputToString = userInput.join('');
-
-   if(isUserInputFloat == true){
+   if(isUserInputFloat){
         userInputToFloat = parseFloat(userInputToString)
        document.getElementById("number").innerHTML = userInputToFloat;
         console.log("Here we are");
